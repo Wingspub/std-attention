96b9a78da7c90a6e07a8c9c8e8486acee2001ace
Wingspub <wingszhangyijie@gmail.com>
2026-08-16 20:13:36 +0800

    feat: 更新Simple的实现

--- models/simplest_transformer.py
+++ models/simplest_transformer.py
@@ -19,7 +19,17 @@ class SimpleSequentialModelV0(nn.Module):
         V = cast(torch.Tensor, self.V_trans(input_seq_embeddings))
 
         # simplest casual matrix
-        A = torch.tril(torch.ones((L, L), device=V.device)) / torch.arange(1, L+1, device=V.device).unsqueeze(1)
+        # A = torch.tril(torch.ones((L, L), device=V.device)) / torch.arange(1, L+1, device=V.device).unsqueeze(1)
+
+        ## rand matrix
+        # A = torch.tril(torch.rand((L, L), device=V.device) + 1e-6)
+        # A = A / A.sum(dim=-1, keepdim=True)
+
+        ## Length decay
+        A = torch.tril(torch.arange(1, 1+L, device=V.device).repeat((L, 1)) + 1e-6)
+        A = A / A.sum(dim=-1, keepdim=True)
+
+
         output = torch.matmul(A, V)
         output = self.output_proj(output)
 
@@ -29,25 +39,45 @@ class SimpleSequentialModelV0(nn.Module):
 class AdvancedSequentialModel(nn.Module):
     def __init__(self, dims: int) -> None:
         super().__init__()
-
-
-
-
-
+        self.W_Q = nn.Linear(dims, dims, bias=False)
+        self.W_K = nn.Linear(dims, dims, bias=False)
+        self.W_V = nn.Linear(dims, dims, bias=False)
+        nn.init.zeros_(self.W_Q.weight)
+        nn.init.zeros_(self.W_K.weight)
+        nn.init.zeros_(self.W_V.weight)
+        self.output_proj = nn.Linear(dims, dims, bias=False)
 
 
     def forward(self, input_embs: torch.Tensor):
-        
+        # input_embs (B, L, d)
+        B, L, _ = input_embs.shape
+        query = cast(torch.Tensor, self.W_Q(input_embs))
+        key = cast(torch.Tensor, self.W_K(input_embs))
+        value = cast(torch.Tensor, self.W_V(input_embs))
 
+        # weight
+        ## V0 std softmax
+        # causal_mask = torch.log(torch.tril(torch.ones((L, L), device=query.device)))
+        # A = torch.softmax(query @ key.transpose(1, 2) + causal_mask, dim=-1)
 
+        ## V0.1 not softmax
+        # A = torch.tril(query @ key.transpose(1, 2) + 1e-6)
+        # A = A / A.sum(dim=-1, keepdim=True)
 
-        ...
+        ## V1
+        A = query @ key.transpose(1, 2) / torch.arange(1, L+1, device=value.device).unsqueeze(1)
+
+        output = A @ value
+        output = self.output_proj(output)
+
+        return output
 
 
 class SimplestBlock(nn.Module):
     def __init__(self, dims: int) -> None:
         super().__init__()
         self.attention = SimpleSequentialModelV0(dims=dims)
+        # self.attention = AdvancedSequentialModel(dims=dims)
 
         self.FFN = nn.Sequential(
             nn.Linear(dims, dims),
@@ -58,7 +88,9 @@ class SimplestBlock(nn.Module):
     def forward(self, input_embs: torch.Tensor):
 
         # Attention
+        # print(self.attention(input_embs), self.attention(input_embs).shape)
         x = input_embs + self.attention(input_embs)
+
 
         # FFN
         output = x + self.FFN(x)
